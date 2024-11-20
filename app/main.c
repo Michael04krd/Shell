da0fa610ed6ea1c767c4d7427a604a4f497ae2bf
Michael04krd <bob.michael0405@mail.ru>
2024-11-20 14:59:39 +0300

    push 7 qests

--- app/main.c
+++ app/main.c
@@ -1,61 +1,102 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
 
-void save_history(char*command) {
+void save_history(char*command) { // функция сохранения истории введенных команд
   FILE *file = fopen("history-txt", "a");
-  if(file == NULL){
+  if(file == NULL){ // проверяем файл на пустоту
     perror("Failed to open, file is empty");
     return;
   }
-  fprintf(file, "%s\n", command);
+  fprintf(file, "%s\n", command);  // записываем в файл последнюю команду
   fclose(file);
 }
 
-int main() {
+
+bool read_input(char *input, size_t size) {  // функция чтения входной строки
+  printf("Enter your string: ");
+  if(fgets(input, size, stdin) == NULL) { // проверка на ошибку чтения. Если fgets вернул NULL, то цикл прерывается, выдавая сообщение Exit
+    printf("Exit\n");
+    return false;
+  }
+  input[strcspn(input, "\n")] = '\0'; // изменения символа новой строки \n на нулевой символ \0
+  return true;
+}
+
+
+bool isEmpty(char *input) { // проверка введенной строки на пустоту, а также на пробелы
+  for(size_t index = 0; index < strlen(input); index++) {
+    if(!isspace(input[index]))
+      return true;
+  }
+  return false;
+}
+
+
+bool process_exit(char *input) {
+  if((strncmp(input, "\\q", 2) == 0) || (strncmp(input, "exit", 4) == 0)) { // Если ввели exit or \q, то выходим
+    return true;
+  }
+  return false;
+}
+
+
+void process_echo(char *input) { // функция-обработка ключевой команды echo, для вывода введенной пользователем строки
+  printf("%s\n", input+5); // вывод на экран строки отступая 5 символов от начала (echo )
+}
+
+
+void enviroment_var(char *input) {  // функция вывода переменной окружения 
+  char *var_name = input + 3;  // пропустили в строке "\e "
+  if(isEmpty(var_name)) {
+    printf("Incorrect name of the environment variable\n");
+    return ;
+  }
+
+  char *var_value = getenv(var_name); // создаем указатель, указывающий на значение переменной окружения
+
+  if(var_value == NULL) {  //Если переменная окружения не найдена
+    printf("Environment variable %s\n not found\n", var_name);
+  } else {  // Если нашли, то вывели
+    printf("Environment variable: %s\n = %s\n\n", var_name, var_value);
+  }
+}
+
+int main() { 
   char input[200];
   
   while(1) {
+    if(!read_input(input, sizeof(input))) {
+      break;
+    }
+
     save_history(input);
-    printf("Enter your string: ");
-    if(fgets(input, sizeof(input), stdin) == NULL) {
-      printf("Exit\n");
+
+    if(process_exit(input)) {
+      printf("You successfully exited\n");
       break;
     }
 
-    input[strcspn(input, "\n")] = 0;
-
-    if(strcmp(input, "exit") == 0 || strcmp(input, "\\q") == 0) {
-      printf("exit\n");
-      break;
-    }
-
-    if (strncmp(input, "echo ", 5) == 0) {
-      printf("%s\n", input+5);
+    if(strncmp(input, "echo ", 5) == 0) { // Функция сравнивает 2 строки и возвращает 0, если равны
+      process_echo(input);
       continue;
     }
 
-    if(strcmp(input, "") == 0){
-      printf("enter correct command dibil\n");
-      continue;
-    }
-
-    printf("String you entered: %s\n", input);
 
     if(strncmp(input, "\\e ", 3) == 0) {
-      char* varName = input+3;
-      varName[strcmp(varName, "\n")] = 0;
-      char* varValue = getenv(varName);
-      if(varValue == NULL) {
-        printf("Var not found: %s\n", varName);
-      }
-      else {
-        printf("%s\n", varValue);
-      }
+      enviroment_var(input);
+      continue;
     }
+
+    if(!isEmpty(input)) {
+      printf("String you entered is empty\n");
+      continue;
+    }
+
+    printf("String you entered %s\n", input);
   }
-
-
   return 0;
 }
 
